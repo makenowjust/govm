b7c855ba77ef99ef6242bd05e006a80ba972605d
TSUYUSATO Kitsune <make.just.on@gmail.com>
2014-07-16 19:49:34 -0700

    tiny fix

--- test/hg/setup.bash
+++ test/hg/setup.bash
@@ -9,7 +9,7 @@ cp $GOVM_SELF_REPO_URL/test/hg/hgrc $GOVM_REPO_URL/.hg/hgrc
 mkdir bin src
 cp $GOVM_SELF_REPO_URL/test/hg/go $GOVM_REPO_URL/bin
 cp $GOVM_SELF_REPO_URL/test/hg/make.bash $GOVM_REPO_URL/src
-hg add src/make.bash
+hg add src/make.bash bin/go
 
-hg commit -m "add make.bash"
+hg commit -m "add make.bash and go"
 hg tag -m "set tag" gotest
